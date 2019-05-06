e7a03edcee39e673d4d2307ab1be31f0ba6c38ce
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-05-06 16:19:25 +0300

    worked on first app

--- first-native-app/App.js
+++ first-native-app/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
