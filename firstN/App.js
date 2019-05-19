6adf33e61201cc01987ac5a49c6d223996202436
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-05-19 17:56:29 +0300

    worked on textInput styles

--- firstN/App.js
+++ firstN/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,12 +17,26 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  state = {
+    placeName: ''
+  }
+
+  placeNameChangeHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  }
+  
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TextInput
+          style={{width: 300, borderColor: 'black', borderWidth: 1}}
+          placeholder="Awesome input"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangeHandler}
+        />
       </View>
     );
   }
@@ -30,19 +44,9 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: 'yellow',
+  }
 });
