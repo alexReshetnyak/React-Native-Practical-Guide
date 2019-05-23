16375d90ae5775b0924de751326bcfe0e46c1242
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-05-23 09:55:09 +0300

    worked with button

--- firstN/App.js
+++ firstN/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -31,12 +31,15 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          style={{width: 300, borderColor: 'black', borderWidth: 1}}
-          placeholder="Awesome input"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangeHandler}
-        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{backgroundColor: 'white', marginBottom: 10, ...styles.placeInput}}
+            placeholder="Awesome input"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangeHandler}
+          />
+          <Button color="#841584" title='ADD'/>
+        </View>
       </View>
     );
   }
@@ -44,9 +47,31 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    justifyContent: 'center',
+    flex: 1,
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
     alignItems: 'center',
     backgroundColor: 'yellow',
+    padding: 20,
+  },
+  inputContainer: {
+    // flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+ 
+    alignItems: 'center',
+    alignContent: 'center'
+
+  },
+  placeInput: {
+    width: '70%',
+    borderWidth: 1,
+    borderColor: 'red'
+  },
+  placeButton: {
+    height: '100%',
+    width: '30%',
+    borderWidth: 1,
+    borderColor: 'red'
   }
 });
