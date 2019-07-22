bd2eeed6bcfc4a391e6da91c2666cc479b03d05e
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-07-22 09:44:19 +0300

    added changes to placeDetail component

--- firstN/src/components/PlaceDetail/PlaceDetail.js
+++ firstN/src/components/PlaceDetail/PlaceDetail.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { Modal, View, Image, Text, Button } from 'react-native'
 
 const PlaceDetail = ({selectedPlace}) => {
-  console.log('SelectedPlace', selectedPlace);
+  const modalContent = selectedPlace ?
+    (
+      <View>
+        <Image source={selectedPlace ? selectedPlace.placeImage : null} />
+        <Text>{selectedPlace ? selectedPlace.name : null}</Text>
+      </View>
+    ) : null;
 
   return (
     <Modal>
       <View>
-        <Image source={selectedPlace ? selectedPlace.placeImage : null} />
-        <Text>{selectedPlace ? selectedPlace.name : null}</Text>
+        {modalContent}
         <View>
-          <Button />
-          <Button />
+          <Button title="Delete" />
+          <Button title="Close" />
         </View>
       </View>
     </Modal>
