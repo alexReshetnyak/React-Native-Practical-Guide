9bf5c7d235f3e2756a246bfce0c908050f586823
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-07-29 16:24:51 +0300

    added top bar navigation

--- firstN/App.js
+++ firstN/App.js
@@ -3,77 +3,54 @@
 // * to resolve watchers issue use:  echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 // ? Debug: react-devtools //////////////// Debug JS Remotely better to use react-native-debbuger
 
-import React, {Component} from 'react';
-import {
-  StyleSheet, 
-  View,
-  Image
-} from 'react-native';
-import { connect } from 'react-redux';
+// import { Provider } from 'react-redux';
+import { Navigation } from "react-native-navigation";
 
-import { PlaceList } from './src/components/PlaceList/PlaceList';
-import { PlaceInput } from './src/components/PlaceInput/PlaceInput';
-import { PlaceDetail } from './src/components/PlaceDetail/PlaceDetail';
-import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions';
+// import configureStore from './src/store/configureStore';
+import { AuthScreen } from '../firstN/src/screens/Auth/Auth';
 
-class App extends Component {
+// const store = configureStore();
 
-  placeAddedHandler = newPlaceName => {
-    this.props.onAddPlace(newPlaceName);
+// * Redux wrapper for App
+// const ReactNativeRedux = () => (
+//   <Provider store={store}>
+//     <AuthScreen />
+//   </Provider>
+// );
+
+Navigation.registerComponent(`navigation.auth.AuthScreen`, () => AuthScreen);
+
+const screens = [{
+  component: {
+    name: "navigation.auth.AuthScreen",
+    options: {
+      topBar: {
+        title: {
+          text: 'Login',
+          color: 'red',
+          fontSize: 18,
+          alignment: 'center'|'fill'
+        },
+        background: {
+          color: '#0079f3'
+        }
+      }
+    }
   }
+}];
 
-  onPlaceSelectedHandler = key => {
-    this.props.onSelectPlace(key);
-  }
 
-  onPlaceDeletedHandler = () => {
-    this.props.onDeletePlace();
-  }
-
-  onModalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail 
-          selectedPlace={this.props.selectedPlace}
-          onItemDeleted={this.onPlaceDeletedHandler}
-          onModalClosed={this.onModalClosedHandler} 
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList 
-          places={this.props.places} 
-          onItemSelected={this.onPlaceSelectedHandler} 
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: 'yellow',
-    padding: 20,
-  }
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        options: {
+          topBar: {
+            visible: true
+          }
+        },
+        children: screens
+      }
+    }
+  });
 });
-
-
-const mapStateToProps = state => ({
-  places:         state.places.places,
-  selectedPlace:  state.places.selectedPlace
-});
-
-const mapDispatchToProps = dispatch => ({
-  onAddPlace:     (name) => dispatch(addPlace(name)),
-  onDeletePlace:      () => dispatch(deletePlace()),
-  onSelectPlace:   (key) => dispatch(selectPlace(key)),
-  onDeselectPlace:    () => dispatch(deselectPlace()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
--- firstN/index.android.js
+++ firstN/index.android.js
@@ -1,38 +1,3 @@
 // * Index js for android platforms (not required)
 
-// import React from 'react';
-// // import { AppRegistry } from 'react-native';
-// import { Provider } from 'react-redux';
-// import { Navigation } from "react-native-navigation";
-
-// import App from './App';
-// // import { name as firstApp } from './app.json';
-// import configureStore from './src/store/configureStore';
-
-// const store = configureStore();
-
-// // * Redux wrapper for App
-// const ReactNativeRedux = () => (
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
-
-// // AppRegistry.registerComponent(firstApp, () => ReactNativeRedux);
-// Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => ReactNativeRedux);
-
-// Navigation.events().registerAppLaunchedListener(() => {
-//   Navigation.setRoot({
-//     root: {
-//       component: {
-//         name: "navigation.playground.WelcomeScreen"
-//       }
-//     }
-//   });
-// });
-
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as firstApp } from './app.json';
-
-AppRegistry.registerComponent(firstApp, () => App);
+import './App';
