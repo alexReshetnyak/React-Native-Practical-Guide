cd4bce48aad3969911af386b6e32a92543dca615
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-09-05 12:04:55 +0300

    pick location added option to add current location

--- firstN/src/components/PickLocation/PickLocation.js
+++ firstN/src/components/PickLocation/PickLocation.js
@@ -5,6 +5,7 @@ import MapView from 'react-native-maps';
 // * Instructions how to fix google maps for android:
 // * 1) Android Studio -> search SDK manager -> SDK tools -> install Google play services
 // * 2) Enable your google api key
+// * 3) Change user permissions to get current location in android -> src -> main -> AndroidManifest
 // * 3) Android Studio -> search AVD manager -> create new device with different version of android (optional)
 
 class PickLocation extends Component {
@@ -17,19 +18,67 @@ class PickLocation extends Component {
         Dimensions.get('window').width / 
         Dimensions.get('window').height * 
         0.0122
-    }
+    },
+    locationChosen: false
   }
 
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coords.latitude,
+      longitude: coords.longitude
+    });
+
+    this.setState(prevState => ({
+      focusedLocation: {
+        ...prevState.focusedLocation,
+        latitude: coords.latitude,
+        longitude: coords.longitude
+      },
+      locationChosen: true
+    }));
+  }
+
+  getLocationHandler = () => {
+    // * navigator - React native global object
+    navigator.geolocation.getCurrentPosition(pos => {
+        const coordsEvent = {
+          nativeEvent: {
+            coordinate: {
+              longitude: pos.coords.longitude,
+              latitude: pos.coords.latitude
+            }
+          }
+        }
+        this.pickLocationHandler(coordsEvent);
+      },
+      err => {
+        console.log(err);
+        alert('Fetching the Position failed, please pick one manually');
+      }
+    )
+  }
+  
+
   render() { 
+    let marker = null;
+    if (this.state.locationChosen) {
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />
+    }
+
     return (
       <View style={styles.container}>
         <MapView
           initialRegion={this.state.focusedLocation}
-          style={styles.map} 
-        />
-        
+          style={styles.map}
+          onPress={this.pickLocationHandler}
+          ref={ref => this.map = ref} // * bind class property map with MapView element
+        >
+          {marker}
+        </MapView>
         <View style={styles.button}>
-          <Button title='Locate me' onPress={() => alert('Pick Location!')}/>
+          <Button title='Locate me' onPress={this.getLocationHandler}/>
         </View>
       </View>
     );
