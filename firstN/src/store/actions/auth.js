7ca22ed7cfa6836be530d9ba7f0f159ea627fcb7
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-09-20 18:56:16 +0300

    worked on sign up

--- firstN/src/store/actions/auth.js
+++ firstN/src/store/actions/auth.js
@@ -1,16 +1,32 @@
 import { TRY_AUTH } from './actionTypes';
+import { API_KEY } from '../../config';
 
-export const tryAuth = authData => ({
-  type: TRY_AUTH,
-  authData
-});
+export const tryAuth = authData => dispatch => {
+  dispatch(authSignup(authData));
+};
 
 
 export const authSignup = authData => async dispatch => {
-  await fetch(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[API_KEY]',
-    {
-      method: 'POST'
-    }
-  );
+  try {
+    const res = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email             : authData.email,
+          password          : authData.password,
+          returnSecureToken : true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    const parsedRes = await res.json();
+    console.log('AUTH RESPONSE:', parsedRes);
+  } catch (error) {
+    console.error(error);
+    alert('Authentication failed, please try again')
+  }
 }
