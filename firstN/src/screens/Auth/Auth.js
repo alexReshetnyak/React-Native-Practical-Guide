9076269820654548102fe4be26e34f6055248bae
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-10-21 09:27:49 +0300

    was remade Auth class to stateless function

--- firstN/src/screens/Auth/Auth.js
+++ firstN/src/screens/Auth/Auth.js
@@ -19,87 +19,71 @@ import { authFormControls } from "./AuthFormControls";
 import { tryAuth, authAutoSignIn } from "../../store/actions";
 import SplashScreen from 'react-native-splash-screen';
 
-class AuthScreen extends Component {
-  state = {
-    viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape",
-    controls: authFormControls,
-    authMode: "login"
-  };
 
-  constructor(props) {
-    super(props);
-    Dimensions.addEventListener("change", this.updateStyles);
-  }
+const AuthScreen = props => {
+  const { isLoading, onTryAuth, onAutoSignIn } = props;
+  const [controls, setControls] = useState(authFormControls);
+  const [authMode, setAuthMode] = useState('login');
+  const [viewMode, setViewMode] = useState(Dimensions.get("window").height > 500 ? "portrait" : "landscape");
 
-  componentDidMount() {
-    this.props.onAutoSignIn();
+  useEffect(() => {
+    // !DEBUG
+    console.log('Component did mount');
+    Dimensions.addEventListener("change", updateStyles);
+    onAutoSignIn();
     SplashScreen.hide();
-  }
+  }, []);
 
-  componentWillUnmount() {
-    Dimensions.removeEventListener("change", this.updateStyles);
-  }
+  useEffect(() => { return () => {
+    // !DEBUG
+    console.log('Component will unmount');
+    Dimensions.removeEventListener("change", updateStyles);
+  }}, []);
 
-  updateStyles = dims => {
-    this.setState({
-      viewMode: dims.window.height > 500 ? "portrait" : "landscape"
+  useEffect(() => {
+    setControls({
+      ...controls,
+      confirmPassword: {
+        ...controls.confirmPassword,
+        valid: validateFormValue(
+          controls["confirmPassword"].value,
+          "confirmPassword",
+          controls
+        )
+      }
     });
+  }, [controls.password.value])
+
+  const updateStyles = dims => {
+    setViewMode(dims.window.height > 500 ? "portrait" : "landscape");
   };
 
-  switchAuthMode = () => {
-    this.setState(prevState => ({
-      authMode: prevState.authMode === "login" ? "signup" : "login"
-    }));
+  const switchAuthMode = () => {
+    setAuthMode(authMode === "login" ? "signup" : "login");
   };
 
-  authHandler = () => {
-    const { controls } = this.state;
+  const handleAuth = () => {
     const authData = Object.keys(controls).reduce((sum, key) => {
        sum[key] = controls[key].value;
        return sum;
     }, {});
     Keyboard.dismiss();
-    this.props.onTryAuth(authData, this.state.authMode);
+    onTryAuth(authData, authMode);
   };
 
-  updateInputState = (key, value) => {
-    this.setState(
-      prevState => ({
-        controls: {
-          ...prevState.controls,
-          [key]: {
-            ...prevState.controls[key],
-            valid: validateFormValue(value, key, prevState.controls),
-            pristine: false,
-            value
-          }
-        }
-      }),
-      () => {
-        if (
-          key === "password" &&
-          !this.state.controls["confirmPassword"].valid
-        ) {
-          this.setState(prevState => ({
-            controls: {
-              ...prevState.controls,
-              confirmPassword: {
-                ...prevState.controls.confirmPassword,
-                valid: validateFormValue(
-                  prevState.controls["confirmPassword"].value,
-                  "confirmPassword",
-                  prevState.controls
-                )
-              }
-            }
-          }));
-        }
+  const updateInputState = (key, value) => {
+    setControls({
+      ...controls,
+      [key]: {
+        ...controls[key],
+        valid: validateFormValue(value, key, controls),
+        pristine: false,
+        value
       }
-    );
+    });
   };
 
-  checkFormValidity = () => {
-    const { controls, authMode } = this.state;
+  const checkFormValidity = () => {
     isValid = Object.keys(controls).every(key => {
       return authMode === "login" && key === "confirmPassword"
         ? true
@@ -108,36 +92,34 @@ class AuthScreen extends Component {
     return isValid;
   };
 
-  render() {
-    const { viewMode, controls, authMode } = this.state;
-    const headingText =
-      viewMode === "portrait" ? (
-        <MainText>
-          <HeadingText style={{ color: "white" }}>
-            Please {authMode === "login" ? " Sign Up" : " Login"}
-          </HeadingText>
-        </MainText>
-      ) : null;
-    const submitButton = this.props.isLoading ? (
+  const headingText = viewMode === "portrait" ? (
+      <MainText>
+        <HeadingText style={{ color: "white" }}>
+          Please {authMode === "login" ? " Sign Up" : " Login"}
+        </HeadingText>
+      </MainText>
+    ) : null;
+
+  const submitButton = isLoading ? (
       <ActivityIndicator />
     ) : (
       <ButtonWithBackground
-        disabled={!this.checkFormValidity()}
-        onPress={this.authHandler}
+        disabled={!checkFormValidity()}
+        onPress={handleAuth}
       >
         Submit
       </ButtonWithBackground>
     );
 
-    return (
-      <ImageBackground
+  return (
+    <ImageBackground
         source={backgroundImage}
         style={authStyles.backgroundImage}
       >
         <KeyboardAvoidingView style={authStyles.container} behavior="height">
           {headingText}
 
-          <ButtonWithBackground onPress={this.switchAuthMode}>
+          <ButtonWithBackground onPress={switchAuthMode}>
             Switch to {authMode === "login" ? " Sign Up" : " Login"}
           </ButtonWithBackground>
 
@@ -145,134 +127,14 @@ class AuthScreen extends Component {
             controls={controls}
             authMode={authMode}
             viewMode={viewMode}
-            onFormChange={this.updateInputState}
+            onFormChange={updateInputState}
           />
 
           {submitButton}
-        </KeyboardAvoidingView>
-      </ImageBackground>
-    );
-  }
-}
-
-// * --------------------------SFC----------------------------------
-
-// const AuthScreen = props => {
-//   const { isLoading, onTryAuth, onAutoSignIn } = props;
-//   const [controls, setControls] = useState(authFormControls);
-//   const [authMode, setAuthMode] = useState('login');
-//   const [viewMode, setViewMode] = useState(Dimensions.get("window").height > 500 ? "portrait" : "landscape");
-
-//   useEffect(() => {
-//     console.log('Component did mount');
-//     Dimensions.addEventListener("change", updateStyles);
-//     onAutoSignIn();
-//     SplashScreen.hide();
-//   }, []);
-
-//   useEffect(() => { return () => {
-//     console.log('Component will unmount');
-//     Dimensions.removeEventListener("change", updateStyles);
-//   }}, []);
-
-//   useEffect(() => {
-//     console.log('Password valid change', controls.password.valid);
-//     !controls["confirmPassword"].valid && setControls({
-//       ...controls,
-//       confirmPassword: {
-//         ...controls.confirmPassword,
-//         valid: validateFormValue(
-//           controls["confirmPassword"].value,
-//           "confirmPassword",
-//           controls
-//         )
-//       }
-//     });
-//   }, [controls.password.valid])
-
-//   const updateStyles = dims => {
-//     setViewMode(dims.window.height > 500 ? "portrait" : "landscape");
-//   };
-
-//   const switchAuthMode = () => {
-//     setAuthMode(authMode === "login" ? "signup" : "login");
-//   };
-
-//   const handleAuth = () => {
-//     const authData = Object.keys(controls).reduce((sum, key) => {
-//        sum[key] = controls[key].value;
-//        return sum;
-//     }, {});
-//     Keyboard.dismiss();
-//     onTryAuth(authData, authMode);
-//   };
-
-//   const updateInputState = (key, value) => {
-//     setControls({
-//       ...controls,
-//       [key]: {
-//         ...controls[key],
-//         valid: validateFormValue(value, key, controls),
-//         pristine: false,
-//         value
-//       }
-//     });
-//   };
-
-//   const checkFormValidity = () => {
-//     isValid = Object.keys(controls).every(key => {
-//       return authMode === "login" && key === "confirmPassword"
-//         ? true
-//         : controls[key].valid;
-//     });
-//     return isValid;
-//   };
-
-//   const headingText = viewMode === "portrait" ? (
-//       <MainText>
-//         <HeadingText style={{ color: "white" }}>
-//           Please {authMode === "login" ? " Sign Up" : " Login"}
-//         </HeadingText>
-//       </MainText>
-//     ) : null;
-
-//   const submitButton = isLoading ? (
-//       <ActivityIndicator />
-//     ) : (
-//       <ButtonWithBackground
-//         disabled={!checkFormValidity()}
-//         onPress={handleAuth}
-//       >
-//         Submit
-//       </ButtonWithBackground>
-//     );
-
-//   return (
-//     <ImageBackground
-//         source={backgroundImage}
-//         style={authStyles.backgroundImage}
-//       >
-//         <KeyboardAvoidingView style={authStyles.container} behavior="height">
-//           {headingText}
-
-//           <ButtonWithBackground onPress={switchAuthMode}>
-//             Switch to {authMode === "login" ? " Sign Up" : " Login"}
-//           </ButtonWithBackground>
-
-//           <AuthForm
-//             controls={controls}
-//             authMode={authMode}
-//             viewMode={viewMode}
-//             onFormChange={updateInputState}
-//           />
-
-//           {submitButton}
-//       </KeyboardAvoidingView>
-//     </ImageBackground>
-//   );
-// };
-
-// * ------------------------END SFC--------------------------------
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+};
 
 
 const mapStateToProps = state => ({
