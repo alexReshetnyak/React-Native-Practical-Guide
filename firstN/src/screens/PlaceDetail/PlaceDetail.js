50b2678d48da373f564fb69b38aba0a6cf8724c0
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-10-23 09:22:10 +0300

    place detail screen  was refactored to sfc

--- firstN/src/screens/PlaceDetail/PlaceDetail.js
+++ firstN/src/screens/PlaceDetail/PlaceDetail.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text, StyleSheet, TouchableOpacity, Platform, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { connect } from 'react-redux';
@@ -8,48 +8,43 @@ import MapView from "react-native-maps";
 import { deletePlace } from '../../store/actions';
 import { SideDrawer } from '../SideDrawer/SideDrawer'
 
-class PlaceDetailScreen extends Component {
-  state = {
-    viewMode: "portrait"
+const placeDetailScreen = props => {
+  const { selectedPlace } = props;
+  const [viewMode, setViewMode] = useState("portrait")
+
+  useEffect(() => {
+    Dimensions.addEventListener("change", updateStyles);
+    return () => {
+      Dimensions.removeEventListener("change", updateStyles);
+    };
+  }, [])
+
+  useEffect(() => {
+    console.log('NavigationEvents: ', Navigation.events());
+    const listener = Navigation.events().registerNavigationButtonPressedListener(
+      ({ buttonId }) => {
+        buttonId === "openSideDrawerButton" &&
+        SideDrawer.showSideDrawer(props.componentId)
+      } 
+    );
+    return () => listener.remove();
+  }, []);
+
+  const updateStyles = dims => {
+    setViewMode(dims.window.height > 500 ? "portrait" : "landscape");
   };
 
-  constructor(props) {
-    super(props);
-    Navigation.events().bindComponent(this);
-    Dimensions.addEventListener("change", this.updateStyles);
-  }
-
-  componentWillUnmount() {
-    Dimensions.removeEventListener("change", this.updateStyles);
-  }
-
-  updateStyles = dims => {
-    this.setState({
-      viewMode: dims.window.height > 500 ? "portrait" : "landscape"
-    });
-  }
-  
-  navigationButtonPressed({ buttonId }) {
-    buttonId === "openSideDrawerButton" &&
-      SideDrawer.showSideDrawer(this.props.componentId);
-  }
-
-  placeDeleteHandler = () => {
-    this.props.onDeletePlace(this.props.selectedPlace.key);
-
+  const placeDeleteHandler = () => {
+    props.onDeletePlace(props.selectedPlace.key);
     // * Remove component from navigation stack
-    Navigation.pop(this.props.componentId);
-  }
-  
-  render() {
-    const { selectedPlace } = this.props;
-    
+    Navigation.pop(props.componentId);
+  };
 
-    return (
-      <View 
+  return (
+    <View 
         style={[
           styles.container, 
-          this.state.viewMode === "portrait" ? styles.portraitContainer : styles.landscapeContainer
+          viewMode === "portrait" ? styles.portraitContainer : styles.landscapeContainer
         ]}
       >
         <View style={styles.placeDetailContainer}>
@@ -83,7 +78,7 @@ class PlaceDetailScreen extends Component {
           </View>
 
           <View>
-            <TouchableOpacity onPress={this.placeDeleteHandler}>
+            <TouchableOpacity onPress={placeDeleteHandler}>
               <View style={styles.deleteButton}>
                 <Icon 
                   size={30} 
@@ -95,8 +90,7 @@ class PlaceDetailScreen extends Component {
           </View>
         </View>
       </View>
-    );
-  }
+  );
 }
 
 const styles = StyleSheet.create({
@@ -138,6 +132,4 @@ const mapDispatchToProps = dispatch => ({
 });
 
 
-PlaceDetailScreen = connect(null, mapDispatchToProps)(PlaceDetailScreen);
-
-export { PlaceDetailScreen };
+export const PlaceDetailScreen = connect(null, mapDispatchToProps)(placeDetailScreen);
